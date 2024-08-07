e7f27cf3ade07d89035673cff98f20d9176a8f4d
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-07 19:39:58 +0300

    first commit

---
+++ Kişiler/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UsersList from './Components/UsersList'
+
+function App() {
+
+
+  return (
+    <div>
+      <UsersList />
+    </div>
+  )
+}
+
+export default App
